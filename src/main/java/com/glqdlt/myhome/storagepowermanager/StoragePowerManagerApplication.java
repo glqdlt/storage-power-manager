21c5c36e182a620db096f6283f7a806d28b264c8
iw.jhun <dlfdnd0725@gmail.com>
2018-10-21 18:34:23 +0900

    init

---
+++ src/main/java/com/glqdlt/myhome/storagepowermanager/StoragePowerManagerApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.myhome.storagepowermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoragePowerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoragePowerManagerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/glqdlt/myhome/storagepowermanager/StoragePowerManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.glqdlt.myhome.storagepowermanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoragePowerManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
